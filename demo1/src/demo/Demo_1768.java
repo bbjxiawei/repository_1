b5c05cfb1b6aa01fc95d92967b8bae6921728fd9
夏伟 <1485625374@qq.com>
2023-07-01 17:51:41 +0800

    20230701_1768

--- demo1/src/demo/Demo_1768.java
+++ demo1/src/demo/Demo_1768.java
@@ -1,12 +1,14 @@
 package demo;
 
-import java.util.Arrays;
-
 public class Demo_1768 {
-    // 给你两个字符串 word1 和 word2 。
-    // 请你从 word1 开始，通过交替添加字母来合并字符串。
-    // 如果一个字符串比另一个字符串长，就将多出来的字母追加到合并后字符串的末尾。
-    // 返回 合并后的字符串 。
+    /**
+     *     给你两个字符串 word1 和 word2 。
+     *     请你从 word1 开始，通过交替添加字母来合并字符串。
+     *     如果一个字符串比另一个字符串长，就将多出来的字母追加到合并后字符串的末尾。
+     *     返回 合并后的字符串 。
+     */
+
+
     // public static String mergeAlternately(String word1, String word2) {
     //     //定义两个指针
     //     int w1 = 0, w2 = 0;
@@ -31,23 +33,21 @@ public class Demo_1768 {
     // }
 
     public static String mergeAlternately(String word1, String word2) {
-        int i = 0;
-        int j = 0;
-        int m = word1.length();
-        int n = word2.length();
+        int i = 0, j = 0;
         StringBuilder sb = new StringBuilder();
-        while (i < m && j < n) {
+        while (i < word1.length() && j < word2.length()) {
             sb.append(word1.charAt(i++));
             sb.append(word2.charAt(j++));
         }
 
-        while (i < m) {
+        while (i < word1.length()) {
             sb.append(word1.charAt(i++));
         }
 
-        while (j < n) {
+        while (j < word2.length()) {
             sb.append(word2.charAt(j++));
         }
+
         return sb.toString();
     }
 
